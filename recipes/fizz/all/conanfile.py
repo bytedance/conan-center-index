20e3cfc9edccb42eb5f9784cf2c968bb42c1c10d
Xianda Ke <kexianda@bytedance.com>
2026-08-02 11:24:18 +0800

    [fizz] fix for 2022.10.31

--- recipes/fizz/all/conanfile.py
+++ recipes/fizz/all/conanfile.py
@@ -14,7 +14,7 @@
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get
+from conan.tools.files import get
 from conan.tools.env import VirtualBuildEnv
 import os
 
@@ -73,10 +73,6 @@ class FizzConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
-
-    def export_sources(self):
-        export_conandata_patches(self)
 
     def generate(self):
         env = VirtualBuildEnv(self)
