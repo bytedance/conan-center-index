fc58e2a44aaed9d78469f7d1ad7693b249da2498
Xianda Ke <kexianda@bytedance.com>
2026-09-04 17:11:08 +0800

    [libgsasl] fix compile error on gcc14/risc-v

--- recipes/libgsasl/all/test_package/test_package.c
+++ recipes/libgsasl/all/test_package/test_package.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     Gsasl *ctx = NULL;
     int rc = gsasl_init (&ctx);
     if (rc != GSASL_OK) {
-        printf(stderr, "gsasl_init failed\n");
+        fprintf(stderr, "gsasl_init failed\n");
         return rc;
     }
     printf("gsasl_init() result = GSASL_OK \n");
