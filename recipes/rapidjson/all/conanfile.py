d1b559984e6a6438caec2017e521b9aff5a11f8f
Xianda Ke <kexianda@bytedance.com>
2026-09-04 17:15:07 +0800

    [rapidjson]: fix compiling error

--- recipes/rapidjson/all/conanfile.py
+++ recipes/rapidjson/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.files import get, copy
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.layout import basic_layout
 import os
 
@@ -24,6 +24,10 @@ class RapidjsonConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True,
                     destination=self.source_folder)
+        apply_conandata_patches(self)
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def package(self):
         copy(self, pattern="license.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
